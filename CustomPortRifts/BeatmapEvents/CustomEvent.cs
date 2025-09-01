dba38ec3140a19a34ec1108c7dcf3a6537303f0f
Lucas LaBuff <lucasbeef00@gmail.com>
2025-09-01 12:26:50 +0200

    allow parameters to be keyed more specifically

--- CustomPortRifts/BeatmapEvents/CustomEvent.cs
+++ CustomPortRifts/BeatmapEvents/CustomEvent.cs
@@ -10,28 +10,33 @@ public abstract class CustomEvent {
     public BeatmapEvent BeatmapEvent { get; private set; } = default;
     public abstract string Type { get; }
 
-    public string GetString(string key) => BeatmapEvent.GetFirstEventDataAsString(key);
+    public string GetString(string key) {
+        if(DoesTypeMatch()) {
+            var value = BeatmapEvent.GetFirstEventDataAsString($"{GetMatchingType()}.{key}");
+            if(!string.IsNullOrWhiteSpace(value)) {
+                return value;
+            }
+        } 
+        return BeatmapEvent.GetFirstEventDataAsString(key);
+    }
     public bool? GetBool(string key) => BeatmapEvent.GetFirstEventDataAsBool(key);
     public int? GetInt(string key) => BeatmapEvent.GetFirstEventDataAsInt(key);
     public float? GetFloat(string key) => BeatmapEvent.GetFirstEventDataAsFloat(key);
 
-    public string[] GetMatchingTypes() {
+    public string GetMatchingType() {
         var typeSegments = $"{Plugin.NAME}.{Type}".ToLowerInvariant().Split('.');
         var beatmapTypes = BeatmapEvent.type.ToLowerInvariant().Split(' ');
-        var matchingTypes = new List<string>();
         for(int i = 0; i < typeSegments.Length; i++) {
             var partialType = string.Join('.', typeSegments[i..]);
             if(beatmapTypes.Contains(partialType)) {
-                matchingTypes.Add(partialType);
+                return partialType;
             }
         }
-        return [.. matchingTypes];
+        return "";
     }
 
-    public void FlagForProcessing() {
-        foreach(var type in GetMatchingTypes()) {
-            BeatmapEvent.AddEventData($"__MODS__.{type}", Plugin.GUID);
-        }
+    public bool DoesTypeMatch() {
+        return !string.IsNullOrWhiteSpace(GetMatchingType());
     }
 
     public bool ShouldProcess() {
@@ -42,13 +47,19 @@ public abstract class CustomEvent {
                 return processor == Plugin.GUID;
             }
         }
-        return true; // no mod has claimed it—mark it for ourselves
+        return true; // no mod has claimed it - flag it for ourselves
     }
 
     public virtual bool IsValid() {
-        return GetMatchingTypes().Length > 0 && ShouldProcess();
+        return DoesTypeMatch() && ShouldProcess();
     }
-    
+
+    public void FlagForProcessing() {
+        if(IsValid()) {
+            BeatmapEvent.AddEventData($"__MODS__.{GetMatchingType()}", Plugin.GUID);
+        }
+    }
+
     public static bool TryParse<T>(BeatmapEvent beatmapEvent, out T setPortraitEvent) where T : CustomEvent, new() {
         setPortraitEvent = new() {
             BeatmapEvent = beatmapEvent

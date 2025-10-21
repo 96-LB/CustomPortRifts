b3c882ff23c2474bf2d91daf86e09098c44b1bd4
Lucas LaBuff <lucasbeef00@gmail.com>
2025-10-21 11:40:13 +0200

    move things to the animator to fix offset bug from manual animation frames

---
+++ CustomPortRifts/Patches/AnimatorPatch.cs
@@ -0,0 +1,111 @@
+﻿using HarmonyLib;
+using Shared.Utilities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using static Shared.Utilities.DataDrivenAnimator;
+
+namespace CustomPortRifts.Patches;
+
+
+public class AnimatorState : State<DataDrivenAnimator, AnimatorState> {
+    public bool HasVibe => Instance.IsValidAnimation("VibePower");
+    public Image? Image => Instance._targetImage;
+    public Vector2 Position {
+        get => Instance._targetImage != null ? Instance._targetImage.rectTransform.anchoredPosition : Vector2.zero;
+        set {
+            if(Instance._targetImage != null) {
+                Instance._targetImage.rectTransform.anchoredPosition = value;
+            }
+        }
+    }
+
+    public Vector2 Offset { get; private set; } = Vector2.zero;
+    public Vector2 OriginalSize { get; private set; } = Vector2.zero;
+
+    public async Task<Dictionary<string, AnimationType>?> PreloadPortrait(Options options) {
+        var temp = Instance._animations;
+        Instance.Configure(options);
+        var animations = Instance._animations;
+        Instance._animations = temp;
+
+        foreach(var animation in animations.Values) {
+            foreach(var frame in animation.Frames) {
+                await frame.SpriteTask; // preload all sprites
+            }
+        }
+
+        return animations;
+    }
+    
+    public void UpdateFade(float fade) {
+        if(Image != null) {
+            Image.color = new(1, 1, 1, 1 - fade);
+        }
+    }
+
+    public void UpdateOffset(Vector2 offset, bool update = true) {
+        Plugin.Log.LogMessage($"Updating offset to {offset}, update={update}");
+        if(update && Image != null) {
+            Image.rectTransform.anchoredPosition += offset - Offset;
+        }
+        Offset = offset;
+    }
+
+    public void UpdatePortrait(Dictionary<string, AnimationType> animations) {
+        Instance._animations = animations;
+        Instance.Refresh();
+    }
+
+    public void RefreshOffset() {
+        if(
+            Image == null
+            || Instance.ActiveAnimationId == null
+            || !Instance._animations.TryGetValue(Instance.ActiveAnimationId, out var frames)
+            || frames.Frames.Count <= 0
+        ) {
+            return;
+        }
+
+        var num = 0.0;
+        AnimationFrame? animationFrame = null;
+        foreach(var frame in frames.Frames) {
+            if(frame.Sprite != null) {
+                animationFrame = frame;
+            }
+
+            num += frame.Duration;
+            if(num > Instance.TargetTime) {
+                break;
+            }
+        }
+
+        var sprite = animationFrame?.Sprite;
+        if(sprite != null) {
+            if(OriginalSize == Vector2.zero) {
+                OriginalSize = Image.rectTransform.sizeDelta;
+            } else {
+                Image.rectTransform.sizeDelta = OriginalSize;
+            }
+
+            if(animationFrame != null) {
+                if(animationFrame.Offset.HasValue) {
+                    var offset = animationFrame.Offset.Value - Position + Offset;
+                    UpdateOffset(offset, true);
+                }
+            }
+        }
+
+    }
+}
+
+[HarmonyPatch(typeof(DataDrivenAnimator))]
+public static class AnimatorPatch {
+    [HarmonyPatch(nameof(DataDrivenAnimator.Refresh))]
+    [HarmonyPrefix]
+    public static void Refresh(DataDrivenAnimator __instance) {
+        var state = AnimatorState.Of(__instance);
+        state.RefreshOffset();
+    }
+}
--- CustomPortRifts/Patches/PortraitViewPatch.cs
+++ CustomPortRifts/Patches/PortraitViewPatch.cs
@@ -19,7 +19,7 @@ public class PortraitData(Dictionary<string, DataDrivenAnimator.AnimationType> a
 public class PortraitViewState : State<RRPortraitView, PortraitViewState> {
     public Dictionary<string, PortraitData> Portraits { get; } = [];
 
-    public DataDrivenAnimator Animator => Instance._dataDrivenAnimator;
+    public AnimatorState Animator => AnimatorState.Of(Instance._dataDrivenAnimator);
 
     public TransitionManager<float> FadeTransition { get; } = new();
     public TransitionManager<PortraitData> PortraitTransition { get; } = new();
@@ -29,11 +29,6 @@ public class PortraitViewState : State<RRPortraitView, PortraitViewState> {
     public async Task<bool> PreloadPortrait(string baseDir, string name) {
         if(Portraits.ContainsKey(name)) {
             Plugin.Log.LogInfo($"Portrait '{name}' is already preloaded.");
-            return false;
-        }
-        
-        if(!Animator) {
-            Plugin.Log.LogWarning($"Failed to preload portrait '{name}' because no data driven animator was found.");
             return false;
         }
 
@@ -48,62 +43,37 @@ public class PortraitViewState : State<RRPortraitView, PortraitViewState> {
             Config = portrait.CustomAnimations ?? new(),
             LoadOrder = ["PerformingNormal", "PerformingWell", "PerformingPoorly", "VibePower"]
         };
-        var temp = Animator._animations;
-        Animator.Configure(options);
-        Portraits[name] = new(Animator._animations, portrait);
-        Animator._animations = temp;
 
-        foreach(var animation in Portraits[name].Animations.Values) {
-            foreach(var frame in animation.Frames) {
-                await frame.SpriteTask; // preload all sprites
-            }
+        Portraits[name] = new([], portrait); // reserve spot
+        var animations = await Animator.PreloadPortrait(options);
+        if(animations == null) {
+            Plugin.Log.LogWarning($"Failed to preload animations for portrait '{name}'.");
+            return false;
         }
+
+        Portraits[name] = new(animations, portrait);
 
         Plugin.Log.LogInfo($"Preloaded portrait '{name}'.");
         return true;
     }
 
     public bool SetPortrait(string name, float startBeat, float duration) {
-        if(!Animator) {
-            Plugin.Log.LogWarning($"Failed to set portrait '{name}' because no data driven animator was found.");
-            return false;
-        }
-
         if(!Portraits.TryGetValue(name, out var portrait)) {
             Plugin.Log.LogWarning($"Portrait '{name}' not found.");
             return false;
         }
 
-        FadeTransition.StartTransition(new FadeTransition(startBeat, duration), UpdateFade);
+        FadeTransition.StartTransition(new FadeTransition(startBeat, duration), Animator.UpdateFade);
         PortraitTransition.StartTransition(new StaticTransition<PortraitData>(startBeat + duration / 2, portrait), UpdatePortrait);
 
         return true;
     }
-
-    public void UpdateFade(float fade) {
-        if(Animator && Animator._targetImage != null) {
-            Animator._targetImage.color = new (1, 1, 1, 1 - fade);
-        }
-    }
         
     public void UpdatePortrait(PortraitData portrait) {
-        if(!Animator) {
-            return;
-        }
-        
-        Animator._animations = portrait.Animations;
-        Instance._hasVibePowerAnimation = Animator.IsValidAnimation("VibePower");
+        Instance._hasVibePowerAnimation = Animator.HasVibe;
         Instance._characterMaskImage.enabled = Instance._characterMask.enabled = !portrait.Metadata.DisableMask;
-        UpdateOffset(portrait.Metadata.OffsetX, portrait.Metadata.OffsetY);
-        Animator.Refresh();
-    }
-
-    public void UpdateOffset(double x, double y, bool update = true) {
-        var offset = new Vector2((float)x, (float)y);
-        if(update && Instance._characterTransform) {
-            Instance._characterTransform.anchoredPosition += offset - Offset;
-        }
-        Offset = offset;
+        Animator.UpdateOffset(new((float)portrait.Metadata.OffsetX, (float)portrait.Metadata.OffsetY));
+        Animator.UpdatePortrait(portrait.Animations);
     }
 
     public void UpdateTransitions(float beat) {
@@ -124,10 +94,14 @@ public static class PortraitViewPatch {
     [HarmonyPatch(nameof(RRPortraitView.ApplyCustomPortrait))]
     [HarmonyPostfix]
     public static void ApplyCustomPortrait(RRPortraitView __instance, ITrackPortrait portraitMetadata) {
-        var state = PortraitViewState.Of(__instance);
+        var animator = __instance._dataDrivenAnimator;
+        if(animator == null) {
+            return;
+        }
+
+        var state = AnimatorState.Of(animator);
         if(portraitMetadata != null) {
-            // set update to false because the base method already applied the offset
-            state.UpdateOffset(portraitMetadata.OffsetX, portraitMetadata.OffsetY, update: false);
+            state.UpdateOffset(new((float)portraitMetadata.OffsetX, (float)portraitMetadata.OffsetY));
         }
     }
 }
